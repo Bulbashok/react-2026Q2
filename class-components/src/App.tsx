1473be6c431096c6e77aeb8883d7d7618d2e9e87
Bulbashok <bulbashok97@gmail.com>
2026-05-03 11:25:10 +0300

    chore: configure dev environment

---
+++ class-components/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1>Class components</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
